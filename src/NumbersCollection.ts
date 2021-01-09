d7b0c638c4531ed980ba9b52ad55e856a4b2a2ac
david11yf29 <david11yf29@gmail.com>
2021-01-10 00:20:05 +0800

    updated

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,6 +1,9 @@
-export class NumbersCollection {
+import { Sorter } from './Sorter';
 
-  constructor(public data: number[]) {};
+export class NumbersCollection extends Sorter {
+  constructor(public data: number[]) {
+    super()
+  };
 
   get length(): number {
     return this.data.length;
